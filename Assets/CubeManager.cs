069a816daea0772595befd4274c528394ff068c2
Mehmetivan <mehmet.ivan2005@gmail.com>
2026-09-14 16:10:36 +0200

    Enemy Spawner

---
+++ Assets/CubeManager.cs
@@ -0,0 +1,44 @@
+//using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+
+    [SerializeField] Enemy enemyToSpawn;
+    [SerializeField] Transform player;
+
+    float nextSpawn = 0;
+
+    [Range(0.1f, 100.0f)]
+    [SerializeField] float SpawnRate;
+    List<Enemy> enemyList = new();
+
+    private void Update()
+    {
+        nextSpawn += SpawnRate * Time.deltaTime;
+        if (nextSpawn >= 1)
+        {
+            SpawnCube();
+            nextSpawn = 0; 
+        }
+
+    }
+
+    public void SpawnCube()
+    {
+        Vector2 randomPosition = Vector2.zero;
+
+        randomPosition.x = Random.Range(-5,5);
+        randomPosition.y = Random.Range(-5,5);
+
+        Enemy newEnemy = Instantiate(enemyToSpawn, randomPosition, Quaternion.identity);
+
+        newEnemy.SetTarget(player);
+
+        enemyList.Add(newEnemy);
+    }
+
+
+}
+
--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,17 +2,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    Transform target;
     [SerializeField] float MovementSpeed;
 
     [SerializeField] private Rigidbody2D body;
 
-
+    public void SetTarget(Transform target) {  this.target = target; }
 
     void Update()
     {
         Vector2 direction = target.position - transform.position;
         direction.Normalize();
+        // Hello!
 
         //transform.position += (Vector3)(direction * MovementSpeed) * Time.deltaTime;
         body.linearVelocity = direction * MovementSpeed;
